cba74527f423223be292c5017e6d3efc94b30430
Vinicius Andrade <75545098+viniciusandrade95@users.noreply.github.com>
2025-07-09 14:24:51 +0100

    Update BusinessHours.js

--- src/components/BusinessHours.js
+++ src/components/BusinessHours.js
@@ -46,7 +46,7 @@ export function BusinessHours() {
     if (tenant?.id) {
       fetchBusinessData();
     }
-  }, [tenant]);
+  }, [tenant, fetchBusinessData]);
 
   const fetchBusinessData = async () => {
     setLoading(true);
